4b4a94db28392870245c72ef91b78e2e25f0c07e
Mikhail Shomov <m@shomov.spb.ru>
2023-01-24 12:36:36 +0300

    Modelling completed

--- modelling/mul64/tb_mul.vhd
+++ modelling/mul64/tb_mul.vhd
@@ -2,6 +2,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.std_logic_arith.all;
 library gaisler;
 use gaisler.noelv.all;
 use gaisler.noelvint.all;
@@ -18,10 +19,10 @@ end tb_mul;
 architecture tb of tb_mul is
     signal clk      :   std_ulogic;       
     signal rstn     :   std_ulogic;
-    signal holdn    :   std_ulogic;
-    signal ctrl     :   std_logic_vector(2 downto 0);
-    signal op1      :   wordx;
-    signal op2      :   wordx;
+    signal holdn    :   std_ulogic := '0';
+    signal ctrl     :   std_logic_vector(2 downto 0) := ext("0", 3);
+    signal op1      :   wordx := ext("0", 32);
+    signal op2      :   wordx := ext("0", 32);
     signal nready   :   std_ulogic;
     signal mresult  :   wordx;
     signal testen   :   std_ulogic := '0';
@@ -30,9 +31,9 @@ begin
 
     clk_generation : process
     begin
-        clk <= '1';
-        wait for 20 ns;
         clk <= '0';
+        wait for 20 ns;
+        clk <= '1';
         wait for 20 ns;
     end process clk_generation;
 
@@ -43,6 +44,42 @@ begin
         stop;
     end process;
 
+    mul_signals : process
+    begin
+        rstn <= '1';
+        
+        wait for 60 ns;
+        holdn <= '1';
+        ctrl <= "001";
+        op1 <= conv_std_logic_vector(16#7EDCBA98#, 32);
+        op2 <= conv_std_logic_vector(16#78ABCDEF#, 32);
+        wait for 80 ns;
+        holdn <= '0';
+        ctrl <= "000";
+        op1 <= conv_std_logic_vector(0, 32);
+        op2 <= conv_std_logic_vector(0, 32);
+        wait for 80 ns;
+        holdn <= '1';
+        ctrl <= "010";
+        op1 <= conv_std_logic_vector(16#78ABCDEF#, 32);
+        op2 <= conv_std_logic_vector(16#7EDCBA98#, 32);
+        wait for 80 ns;
+        holdn <= '0';
+        ctrl <= "000";
+        op1 <= conv_std_logic_vector(0, 32);
+        op2 <= conv_std_logic_vector(0, 32);
+        wait for 80 ns;
+        holdn <= '1';
+        ctrl <= "011";
+        op1 <= conv_std_logic_vector(16#7EDCBA98#, 32);
+        op2 <= conv_std_logic_vector(16#78ABCDEF#, 32);
+        wait for 80 ns;
+        holdn <= '0';
+        ctrl <= "000";
+        op1 <= conv_std_logic_vector(0, 32);
+        op2 <= conv_std_logic_vector(0, 32);
+        wait for 20 ns;
+    end process mul_signals;
 
 
     UUT : entity gaisler.mul64 port map (clk, rstn, holdn, ctrl, op1, op2, nready, mresult, testen, testrst);
